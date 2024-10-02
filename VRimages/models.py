e64785b5dbc6a8e021f9b7a7c52e656a614235ca
Michael Obadun <r65esxbn@students.codeinstitute.net>
2024-10-02 03:31:23 +0000

    In my models file i changed the banner to accept Cloudinary as its field type and ran migrations

--- VRimages/models.py
+++ VRimages/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
 class Post(models.Model):
@@ -7,7 +8,7 @@ class Post(models.Model):
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
     User, on_delete=models.CASCADE, related_name="blog_posts")
-    banner = models.ImageField(default='fallback.png', blank=False)
+    banner = CloudinaryField('image', default='placeholder')
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
