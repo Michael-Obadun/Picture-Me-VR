44a74106d66ed3fcb7acfa364e6e4071a698f55c
Michael Obadun <r65esxbn@students.codeinstitute.net>
2024-10-02 02:17:22 +0000

    created index.html file in vrimages/templates folder and added some html. In vrimages/views file i changed the template to be displayed ro index.html tempalte. Also due to error removed static css and js links from the base html the i added earlier

--- VRimages/views.py
+++ VRimages/views.py
@@ -6,4 +6,6 @@ from .models import Post
 
 class PostList(generic.ListView):
     queryset = Post.objects.all()
-    template_name = "post_list.html"
+    template_name = "VRimages/index.html"
+
+    
